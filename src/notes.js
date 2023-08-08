55d56cd355b18554f4400253b9998ae004d9ab40
SushantDahal <SushanDahal246@gmail.com>
2023-08-08 14:16:35 +0545

    adding map fucntion

---
+++ src/notes.js
@@ -0,0 +1,33 @@
+const notes = [
+  {
+    key: 1,
+    title: "Deligation",
+    content:
+      "Q. How many programmer does it take to change a light bulb? A. none-Its a hardware problem",
+  },
+  {
+    key: 2,
+    title: "Loops",
+    content:
+      "Q. How to keep a programmer to keep shower forever. show hin the shampoo bottle instruction: lather.rinse.repeat",
+  },
+  {
+    key: 3,
+    title: "Array",
+    content:
+      "Q. Why did the programmer quit his job? A. Because he didn't get array",
+  },
+  {
+    key: 4,
+    title: "Hardware vs Software",
+    content:
+      "Q. Whats the difference between hardware and software? A. You can hit your hardware with a hammer but you cannot curse your software",
+  },
+  {
+    key: 5,
+    title: "Big Ideas",
+    content: "Eat more MOMO",
+  },
+];
+
+export default notes;
